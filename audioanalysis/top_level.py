d4984aabf67b2f428fcb127165c17712236536bf
jpalpant <jbp25@duke.edu>
2015-11-17 18:12:52 -0500

    Develop an initial single-screen GUI for selecting key motif
    This stage added a folder of icons (audioanalysis/icons), a Qt file
    (audioanalyis/main.ui) and the compiled version of that file
    (audioanalysis/window.py).  top_level.py has also been changed to run
    the new GUI in a minimal state
    
    This commit leaves a blank, default Widget for what will be a matplotlib
    Figure
    
    Uses PyQt4.uic to import the .ui file directly
    
    Change-Id: Ic9bc0e7167fd63ea3a7fa9fd166da32358c43a37
    Signed-off-by: jpalpant <jbp25@duke.edu>

--- audioanalysis/top_level.py
+++ audioanalysis/top_level.py
@@ -5,35 +5,48 @@ Created on Nov 16, 2015
 
 Copyright 2015 Justin Palpant
 
-This file is part of Foobar.
+This file is part of the Jarvis Lab Audio Analysis program.
 
-Foobar is free software: you can redistribute it and/or modify it under the
+Audio Analysis is free software: you can redistribute it and/or modify it under the
 terms of the GNU General Public License as published by the Free Software
 Foundation, either version 3 of the License, or (at your option) any later
 version.
 
-Foobar is distributed in the hope that it will be useful, but WITHOUT ANY
+Audio Analysis is distributed in the hope that it will be useful, but WITHOUT ANY
 WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
 PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with
-Foobar. If not, see http://www.gnu.org/licenses/.
+Audio Analysis. If not, see http://www.gnu.org/licenses/.
 '''
 
-import matplotlib
 import freqanalysis
-import PyQt4
+from PyQt4.uic import loadUiType
+ 
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt4agg import (
+    FigureCanvasQTAgg as FigureCanvas)
 
-class AudioGUI():
+Ui_MainWindow, QMainWindow = loadUiType('main.ui')
 
-    def __init__(self):
-        
-    
+class AudioGUI(Ui_MainWindow, QMainWindow):
+
+    def __init__(self, ):
+        super(AudioGUI, self).__init__()
+        self.setupUi(self)
+
+
+
 
 def main():
-
-    pass
-
+    import sys
+    from PyQt4 import QtGui, QtCore
+     
+    app = QtGui.QApplication(sys.argv)
+    main = AudioGUI()
+    main.show()
+    
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
     main()
