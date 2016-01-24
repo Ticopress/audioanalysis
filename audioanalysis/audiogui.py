3bebd346957924e1511029a6dbabb1e460b98c3a
jpalpant <jbp25@duke.edu>
2016-01-24 02:02:45 -0500

    Add results from another neural net test
    Increasing the sample size to 15k per class with a large convolutional
    neural net took more time (and I ran it for 5 epochs) but resulted in a
    decent result: 0.8075 validation accuracy.
    
    Next I'm going to replace the samples_per_class mechanism - I want it to
    train on ALL classified samples and select a subset of the noise samples
    to us.

--- audioanalysis/audiogui.py
+++ audioanalysis/audiogui.py
@@ -113,9 +113,9 @@ class AudioGUI(Ui_MainWindow, QMainWindow):
         #Initialize the collection of assorted parameters
         #Not currently customizable, maybe will make interface later
         defaultlayers = [
-                {'type':'Convolution2D', 'args':(16,6,1,), 'kwargs':{'border_mode':'same'}},
+                {'type':'Convolution2D', 'args':(32,6,1,), 'kwargs':{'border_mode':'same'}},
                 {'type':'Activation', 'args':('relu',)},
-                {'type':'Convolution2D', 'args':(16,6,1,)},
+                {'type':'Convolution2D', 'args':(32,6,1,)},
                 {'type':'Activation', 'args':('relu',)},
                 {'type':'MaxPooling2D', 'kwargs':{'pool_size':(2,1,)}},
                 {'type':'Dropout', 'args':(0.25,)},
@@ -132,7 +132,7 @@ class AudioGUI(Ui_MainWindow, QMainWindow):
                        'process_chunk_s':30, 'layers':defaultlayers, 
                        'loss':'categorical_crossentropy', 'optimizer':'adadelta',
                        'min_dur':1.0, 'max_dur':5.0, 'smooth_gap':0.075,
-                       'min_freq':440.0, 'train_per_class':15000, 'epochs':3,
+                       'min_freq':440.0, 'train_per_class':15000, 'epochs':5,
                        'batch_size':50, 'validation_split':0.33
                        }
         
