2515aa4323666c8260ea122d4f7576b0d472a0e0
jpalpant <jbp25@duke.edu>
2016-02-09 17:39:26 -0500

    Redefine BGThread to take an argument-less function and a name

--- audioanalysis/threadsafety.py
+++ audioanalysis/threadsafety.py
@@ -3,38 +3,22 @@ Created on Feb 9, 2016
 
 @author: justinpalpant
 '''
-from contextlib import contextmanager
-import sys
 from PyQt4 import QtCore
 
-@contextmanager
-def stdout_redirector(stream):
-    old_stdout = sys.stdout
-    sys.stdout = stream
-    try:
-        yield
-    finally:
-        sys.stdout = old_stdout
 
 class BGThread(QtCore.QThread):
-    #say = QtCore.pyqtSignal(str)
     #QThread also defines started and finished as void-type signals
     
-    def __init__(self, fn, *args, **kwargs):
+    def __init__(self, fn, name=''):
         super(BGThread, self).__init__()
         self.function = fn
-        self.args = args
-        self.kwargs = kwargs
+        self.name = name
 
     def __del__(self):
         self.wait()
 
     def run(self):
-        self.function(*self.args, **self.kwargs)
-
-    @property
-    def name(self):
-        return self.function.func_name
+        self.function()
 
 class SignalStream(QtCore.QObject):
     write_signal = QtCore.pyqtSignal(str)
